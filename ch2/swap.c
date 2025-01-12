0c3a493119fd1e89f5ba9b0ff96893acb8b10cb4
notreallydan <183784817+notreallydan@users.noreply.github.com>
2025-01-12 16:20:39 +1100

    added pointer to swap.c

--- ch2/swap.c
+++ ch2/swap.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int a, int b) {
-    int t = a;
-    a = b;
-    b = t;
-    printf("swap: a = %d, b = %d\n", a, b);
+void swap(int *pa, int *pb) {
+    int t = *pa;
+    *pa = *pb;
+    *pb = t;
 }
 
 int main() {
     int a = 21;
     int b = 17;
-    swap(a, b);
+    swap(&a, &b);
     printf("main: a = %d, b = %d\n", a, b);
     return EXIT_SUCCESS;
 }
